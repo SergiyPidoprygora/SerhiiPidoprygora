3a560ae20281c080c32924d7faf1accd922f2183
SergiyPidoprygora <Sergiy.Pidoprygora@kyivstar.net>
2023-05-31 21:36:50 +0300

    Test

--- Work.java
+++ Work.java
@@ -1,6 +1,19 @@
+import com.sun.jdi.IntegerValue;
+
+/* autoboxing by metod */
 public class Work {
-    public static void main(String[] args) {
-
-
+  public static void main(String[] args){
+      Integer a = 5;
+      int b = 40;
+      int result = Integer.valueOf(a+b);
+      System.out.println(result);
+      System.out.println( a == b);
+      System.out.println(a+b==50);
+      int c = 40;
+      short k = 10;
+      int result1 = Integer.valueOf(c*k-b/a);
+      System.out.println(result1);
+      System.out.println(k*c==400);
     }
+
 }
--- home.java
+++ home.java
@@ -1,5 +1,16 @@
 public class home {
     public static void main(String[] args) {
-
+        System.out.println("Home work");
+        System.out.println("Додавання");
+        int c = 6000;
+        int k = 2000;
+        System.out.println(c + k);
+        System.out.println("Віднімання");
+        System.out.println(c - k);
+        System.out.println("Ділення");
+        System.out.println(c / k);
+        System.out.println("Множення");
+        System.out.println(c * k);
     }
 }
+
